46116f2264e15e2786e6299e8b9020963c387eba
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-04-13 21:17:35 -0500

    v1.0.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const links = [
+    { href: '/', label: 'Home' },
+    { href: '/pokemons', label: 'Pokedex' },
+    { href: '/legendaries', label: 'Legendaries' },
+    { href: '/documentation', label: 'Documentation' },
+];
 
 export default function Navbar() {
+    
+    const router = useRouter();
+
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-slate-950">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-slate-900">
             <div className="flex items-center justify-between h-16  w-[90%] m-auto max-w-[1200px]">
                 <Link href={'/'}>
                     <img
@@ -14,18 +25,18 @@ export default function Navbar() {
 
                 <nav>
                     <ul className="flex gap-2">
-                        <li className="px-4 py-2 border-b-2 border-b-transparent hover:border-b-[#FFCC01] transition-colors">
-                            <Link href={'/'}>Home</Link>
-                        </li>
-                        <li className="px-4 py-2 border-b-2 border-b-transparent hover:border-b-[#FFCC01] transition-colors">
-                            <Link href={'/pokemons'}>Pokedex</Link>
-                        </li>
-                        <li className="px-4 py-2 border-b-2 border-b-transparent hover:border-b-[#FFCC01] transition-colors">
-                            <Link href={'/pokemons'}>Legendaries</Link>
-                        </li>
-                        <li className="px-4 py-2 border-b-2 border-b-transparent hover:border-b-[#FFCC01] transition-colors">
-                            <Link href={'/pokemons'}>Documentation</Link>
-                        </li>
+                        {links.map(({ href, label }) => (
+                            <li
+                                key={`${href}${label}`}
+                                className={`py-2 border-b-2  ${
+                                    router.asPath === href
+                                        ? 'border-b-[#FFCC01]'
+                                        : 'border-b-transparent'
+                                } hover:border-b-[#FFCC01] transition-colors`}
+                            >
+                                <Link href={href} className="px-2 py-2">{label}</Link>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
